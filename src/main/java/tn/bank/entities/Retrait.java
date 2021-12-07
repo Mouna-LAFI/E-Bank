b86fb85394aff4750a0690552afd8bdbaf697b02
Mouna-LAFI <ellafimouna1@gmail.com>
2021-12-08 00:09:25 +0100

    not working yet

--- src/main/java/tn/bank/entities/Retrait.java
+++ src/main/java/tn/bank/entities/Retrait.java
@@ -1,5 +1,21 @@
 package tn.bank.entities;
 
-public class Retrait {
+import java.util.Date;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+@Entity
+@DiscriminatorValue("R")
+public class Retrait extends Operation{
+
+	public Retrait() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Retrait(Date dateOperation, double montant, Compte compte) {
+		super(dateOperation, montant, compte);
+		// TODO Auto-generated constructor stub
+	}
 
 }
